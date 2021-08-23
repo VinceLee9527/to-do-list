baa628994e953ddbab912db9fa19276f82967d4d
VinceLee9527 <vlee9527@gmail.com>
2021-08-24 00:43:00 +0800

    added handlebars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require('mongoose')
 const app = express()
 
+const exphbs = require('express-handlebars')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -14,8 +15,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send('hello bitches')
+  res.render('index')
 })
 
 app.listen(3000, () => {
