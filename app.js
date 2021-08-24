bfc172b912dff9b803283eacef8a3ea57f1bbfea
VinceLee9527 <vlee9527@gmail.com>
2021-08-24 22:10:51 +0800

    Added name intake form

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 
 const Todo = require('./models/todo')
 
+const bodyParser = require('body-parser')
+
 const exphbs = require('express-handlebars')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -20,12 +22,29 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
 })
 
 app.listen(3000, () => {
